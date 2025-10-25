323fc73ca6608cdbca56d940b0b97c51270f1507
Alex Star <sonofagronom@gmail.com>
2025-10-26 01:21:17 +0700

    1.5.6 - исправления по миграциям

--- backend/apps/rooms/migrations/0003_recording.py
+++ backend/apps/rooms/migrations/0003_recording.py
@@ -2,6 +2,7 @@
 
 from django.db import migrations, models
 import django.db.models.deletion
+import django.core.validators
 import uuid
 import apps.rooms.recording_models
 
@@ -17,6 +18,8 @@ class Migration(migrations.Migration):
             name='Recording',
             fields=[
                 ('id', models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, serialize=False)),
+                ('room_id', models.CharField(db_index=True, max_length=255, verbose_name='Room ID')),
+                ('created_by_id', models.CharField(blank=True, max_length=255, null=True, verbose_name='Created By ID')),
                 ('started_at', models.DateTimeField(auto_now_add=True)),
                 ('stopped_at', models.DateTimeField(blank=True, null=True)),
                 ('file', models.FileField(blank=True, null=True, upload_to=apps.rooms.recording_models.recording_upload_path, validators=[django.core.validators.FileExtensionValidator(allowed_extensions=['webm', 'mp4', 'mkv'])])),
@@ -31,8 +34,6 @@ class Migration(migrations.Migration):
                 ('password', models.CharField(blank=True, max_length=255)),
                 ('view_count', models.IntegerField(default=0)),
                 ('download_count', models.IntegerField(default=0)),
-                ('room', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='recordings', to='rooms.room')),
-                ('started_by', models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='started_recordings', to='rooms.roomparticipant')),
             ],
             options={
                 'ordering': ['-started_at'],
@@ -40,7 +41,7 @@ class Migration(migrations.Migration):
         ),
         migrations.AddIndex(
             model_name='recording',
-            index=models.Index(fields=['room', 'started_at'], name='rooms_recor_room_id_idx'),
+            index=models.Index(fields=['room_id', 'started_at'], name='rooms_recor_room_id_started_idx'),
         ),
         migrations.AddIndex(
             model_name='recording',
